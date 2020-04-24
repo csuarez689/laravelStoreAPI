02cbe86511da3754931356a2b72a2d966f6dd1be
csuarez689 <csuarez689@gmail.com>
2020-04-24 00:26:58 -0300

    factories ready!

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -15,14 +15,22 @@ use Illuminate\Support\Str;
 | your application. Factories provide a convenient way to generate new
 | model instances for testing / seeding your application's database.
 |
-*/
+ */
 
 $factory->define(User::class, function (Faker $faker) {
+    static $password;
+    $verified = $faker->randomElement([User::VERIFIED_USER, User::UNVERIFIED_USER]);
+    $email_verified_at = ($verified) ? now() : null;
+    $verification_token = ($verified) ? User::generateVerificationCode() : null;
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
-        'email_verified_at' => now(),
-        'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+        'email_verified_at' => $email_verified_at,
+        'password' => $password ?: $password = bcrypt('secret'),
         'remember_token' => Str::random(10),
+        'verified' => $verified,
+        'verification_token' => $verification_token,
+        'admin' => $faker->randomElement([User::REGULAR_USER, User::ADMIN_USER]),
     ];
 });
