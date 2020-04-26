7af527b54c7743fb0fdee624f34dc2cdb7bdb3d1
csuarez689 <csuarez689@gmail.com>
2020-04-26 14:44:35 -0300

    added laravel global scopes and declaration on boot models

--- app/Buyer.php
+++ app/Buyer.php
@@ -2,10 +2,21 @@
 
 namespace App;
 
+use App\Scopes\BuyerScope;
+
 class Buyer extends User
 {
 
     public static $modelName = 'comprador';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        //se agrega scope para obtener aquellos usuarios que poseen transacciones
+        //los cuales corresponderian a compradores
+        static::addGlobalScope(new BuyerScope);
+    }
 
     public function transactions()
     {
--- app/Seller.php
+++ app/Seller.php
@@ -2,9 +2,20 @@
 
 namespace App;
 
+use App\Scopes\SellerScope;
+
 class Seller extends User
 {
     public static $modelName = 'vendedor';
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        //se agrega scope para obtener aquellos usuarios que poseen transacciones
+        //los cuales corresponderian a compradores
+        static::addGlobalScope(new SellerScope);
+    }
 
     public function products()
     {
