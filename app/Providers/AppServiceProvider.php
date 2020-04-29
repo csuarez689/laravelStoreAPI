591f9ecf9245788b88a83129f4f08dd34d98923a
csuarez689 <csuarez689@gmail.com>
2020-04-29 01:03:49 -0300

    event for manage quantity and status of a product

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use App\Product;
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,6 +16,13 @@ class AppServiceProvider extends ServiceProvider
     public function register()
     {
         Schema::defaultStringLength(191);
+        //event listener quantity of product changes
+        Product::updated(function($product){
+            if($product->quantity==0 && $product->isAvailable()){
+                $product->status=Product::UNAVAILABLE_PRODUCT;
+                $product->save();
+            }
+        })
     }
 
     /**
