11102009a294ba3c18ffa854939c8d6942ad442b
csuarez689 <csuarez689@gmail.com>
2020-04-30 15:58:06 -0300

    added middleware name app signature

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
      */
 
-    'name' => env('APP_NAME', 'Laravel'),
+    'name' => env('APP_NAME', 'RESTFulAPI-Store'),
 
     /*
     |--------------------------------------------------------------------------
@@ -67,7 +67,7 @@ return [
     |
      */
 
-    'timezone' => '	America/Argentina/San_Luis',
+    'timezone' => 'America/Argentina/San_Luis',
 
     /*
     |--------------------------------------------------------------------------
