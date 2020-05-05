b23be06b9e59906ad913b060379b21e8c344a694
csuarez689 <csuarez689@gmail.com>
2020-05-05 03:37:14 -0300

    auth generated, improvements for html and json responses

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -2,17 +2,22 @@
 
 namespace App\Exceptions;
 
-use App\Traits\ApiHandlerExceptionResponser;
+use App\Traits\ApiResponser;
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Auth\AuthenticationException;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Session\TokenMismatchException;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
-
-    use ApiHandlerExceptionResponser;
-
+    use ApiResponser;
     /**
      * A list of the exception types that are not reported.
      *
@@ -56,8 +61,55 @@ class Handler extends ExceptionHandler
      */
     public function render($request, Exception $exception)
     {
-        //trait manejador de excepciones con respuestas json y rendirizacion si se encuentra en modo dev
-        return $this->createErrorResponse($request, $exception);
+//respuesta para errorers de validacion
+        if ($exception instanceof ValidationException) {
+            return $this->convertValidationExceptionToResponse($exception, $request);
+        }
+//respuesta modelo o registro no encontrado
+        if ($exception instanceof ModelNotFoundException) {
+            $name = $exception->getModel()::$modelName;
+            return $this->errorJsonResponse("No existe un registo de {$name} con el identificador especificado", 404);
+        }
+//respuesta error en url ingresada
+        if ($exception instanceof NotFoundHttpException) {
+            return $this->errorJsonResponse('La URL ingresada no es valida', 404);
+        }
+//respuesta error de autenticacion de usuario
+        if ($exception instanceof AuthenticationException) {
+            return $this->unauthenticated($request, $exception);
+        }
+//respuesta error permisos insuficientes
+        if ($exception instanceof AuthorizationException) {
+            return $this->errorJsonResponse($exception->getMessage(), 403);
+        }
+//respuesta error metodo no valido en peticion http
+        if ($exception instanceof MethodNotAllowedHttpException) {
+            return $this->errorJsonResponse('El metodo especificado para la peticion es invalido', 405);
+        }
+//respuestas generales http exceptions
+        if ($exception instanceof HttpException) {
+            return $this->errorJsonResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+//manejo de errores en consultas por contraint foreign key
+        if ($exception instanceof QueryException) {
+            $errorCode = $exception->errorInfo[1];
+            if ($errorCode == 1451) {
+                return $this->errorJsonResponse('No se puede eliminar este recurso. Existe informacion relacionada con el mismo', 409);
+            }
+        }
+//Manejo de errores tokens csrf
+        if ($exception instanceof TokenMismatchException) {
+            return redirect()->back()->withInput($request->input());
+        }
+
+//renderizacion de todos los errores en vista -- para desarrollo debug=true
+        if (config('app.debug')) {
+            return parent::render($request, $exception);
+        }
+
+//resto de errores genericos json -- para produccion with debug=false
+        //unexpected exception
+        return $this->errorJsonResponse('Ocurrió un error inesperado. Intente más tarde', 500);
     }
 
     /**
@@ -71,6 +123,14 @@ class Handler extends ExceptionHandler
     protected function convertValidationExceptionToResponse(ValidationException $e, $request)
     {
         $errors = $e->validator->errors()->getMessages();
+
+        if ($this->isFrontend($request)) {
+            return $request->ajax() ? response()->json($errors, 422) : redirect()
+                ->back()
+                ->withInput($request->input())
+                ->withErrors($errors);
+        }
+
         return $this->errorJsonResponse($errors, 422);
     }
 
@@ -84,6 +144,15 @@ class Handler extends ExceptionHandler
     //sobreescritura
     public function unauthenticated($request, AuthenticationException $exception)
     {
+        if ($this->isFrontend($request)) {
+            return redirect()->guest('login');
+        }
         return $this->errorJsonResponse('Unauthenticated', 401);
+    }
+
+    private function isFrontend($request)
+    {
+        $isWebRoute = collect($request->route()->middleware())->contains('web');
+        return $request->acceptsHtml() && $isWebRoute;
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,5 +9,31 @@
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
+// Authentication Routes...
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login');
+Route::post('logout', 'Auth\LoginController@logout')->name('logout');
+
+// Password Reset Routes...
+Route::get('password/reset', 'Auth\ForgotPasswordController@showLinkRequestForm')->name('password.request');
+Route::post('password/email', 'Auth\ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+Route::get('password/reset/{token}', 'Auth\ResetPasswordController@showResetForm')->name('password.reset');
+Route::post('password/reset', 'Auth\ResetPasswordController@reset')->name('password.update');
+
+// Password Confirmation Routes...
+Route::get('password/confirm', 'Auth\ConfirmPasswordController@showConfirmForm')->name('password.confirm');
+Route::post('password/confirm', 'Auth\ConfirmPasswordController@confirm');
+
+// Email Verification Routes...
+Route::get('email/verify', 'Auth\VerificationController@show')->name('verification.notice');
+Route::get('email/verify/{id}/{hash}', 'Auth\VerificationController@verify')->name('verification.verify');
+Route::post('email/resend', 'Auth\VerificationController@resend')->name('verification.resend');
+
+Route::get('/home', 'HomeController@index')->name('home');
+
+//Pagina principal
+Route::get('/', function () {
+    return view('welcome');
+})->middleware('guest');
